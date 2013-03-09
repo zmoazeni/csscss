d2100bc20c1d7a255aeb54b896d198609e2ad186
Zach Moazeni <zach.moazeni@gmail.com>
2013-03-09 01:22:08 -0500

    Implements derivative declarations
    A derivative declaration is one that comes from a parent. Usually
    parsed from a shorthand declaration.

--- lib/csscss/types.rb
+++ lib/csscss/types.rb
@@ -1,15 +1,62 @@
 module Csscss
-  class Declaration < Struct.new(:property, :value)
+  class Declaration < Struct.new(:property, :value, :parent)
     def self.from_csspool(dec)
       new(dec.property.to_s, dec.expressions.join(" "))
+    end
+
+    def self.from_parser(property, value)
+      new(property.to_s, value.to_s.downcase.strip)
+    end
+
+    def derivative?
+      !parent.nil?
+    end
+
+    def without_parent
+      if derivative?
+        dup.tap do |duped|
+          duped.parent = nil
+        end
+      else
+        self
+      end
+    end
+
+    def ==(other)
+      if derivative?
+        without_parent == other
+      else
+        super(other.respond_to?(:without_parent) ? other.without_parent : other)
+      end
+    end
+
+    def hash
+      derivative? ? without_parent.hash : super
+    end
+
+    def eql?(other)
+      hash == other.hash
     end
 
     def <=>(other)
       property <=> other.property
     end
 
+    def >(other)
+      self == other.parent
+    end
+
+    def <(other)
+      other > self
+    end
+
     def to_s
-      "#{property}: #{value}"
+      base = "#{property}: #{value}"
+      if parent
+        "#{base} (parent: #{parent})"
+      else
+        base
+      end
     end
 
     def inspect
---
+++ test/csscss/types_test.rb
@@ -0,0 +1,59 @@
+require "test_helper"
+
+module Csscss
+  describe Declaration do
+    it "< and > checks parent" do
+      dec1 = Declaration.new("background", "#fff")
+      dec2 = Declaration.new("background", "#fff")
+      dec3 = Declaration.new("background-color", "#fff", dec1)
+      dec4 = Declaration.new("background-color", "#fff", nil)
+
+      dec1.must_be :>, dec3
+      dec3.must_be :<, dec1
+
+      dec1.wont_be :>, dec2
+      dec1.wont_be :>, dec4
+      dec4.wont_be :>, dec1
+    end
+
+    it "is a derivative if it has parent" do
+      dec1 = Declaration.new("background", "#fff")
+      dec1.wont_be :derivative?
+      Declaration.new("background-color", "#fff", dec1).must_be :derivative?
+    end
+
+    it "ignores parents when checking equality" do
+      dec1 = Declaration.new("background", "#fff")
+      dec2 = Declaration.new("background-color", "#fff", dec1)
+      dec3 = Declaration.new("background-color", "#fff", nil)
+
+      dec1.wont_equal dec2
+      dec2.wont_equal dec1
+
+      dec2.must_equal dec3
+      dec3.must_equal dec2
+
+      dec2.hash.must_equal dec3.hash
+      dec3.hash.must_equal dec2.hash
+      dec3.hash.wont_equal dec1.hash
+
+      dec2.must_be :eql?, dec3
+      dec3.must_be :eql?, dec2
+      dec2.wont_be :eql?, dec1
+    end
+
+    it "derivatives are handled correctly in a hash" do
+      dec1 = Declaration.new("background", "#fff")
+      dec2 = Declaration.new("background-color", "#fff", dec1)
+      dec3 = Declaration.new("background-color", "#fff", nil)
+
+      h = {}
+      h[dec2] = false
+      h[dec3] = true
+
+      h.keys.size.must_equal 1
+      h[dec2].must_equal true
+      h[dec3].must_equal true
+    end
+  end
+end
