fe8ad711a2ebb713a4351ed2566b635ca3cd101a
Martin Kuckert <martin@curlybracket.de>
2013-04-10 09:59:18 +0200

    Parse compass config file, if available

--- lib/csscss/cli.rb
+++ lib/csscss/cli.rb
@@ -26,6 +26,10 @@ module Csscss
             exit 1
           end
 
+          if File.exist?("config.rb") and @compass
+            Compass.add_configuration("config.rb")
+          end
+
           sass_options = {cache:false}
           sass_options[:load_paths] = Compass.configuration.sass_load_paths if @compass
           begin
